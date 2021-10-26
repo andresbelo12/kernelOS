18959b3d94f03af0f5fdd10bf4060dcd58ab0996
Andres Lopez <jalopezb@eafit.edu.co>
2021-10-26 16:22:20 -0500

    Fix read message

--- handler/communication.go
+++ handler/communication.go
@@ -9,19 +9,19 @@ import (
 	"github.com/andresbelo12/KernelOS/model"
 )
 
-func ReadMessage(connection *net.Conn)(message model.Message){
+func ReadMessage(connection *net.Conn) (message model.Message) {
 	buffer := make([]byte, 1024)
+	bufferLength, err := (*connection).Read(buffer)
 
-	_, err := (*connection).Read(buffer)
 	if err != nil {
 		if err != io.EOF {
 			fmt.Println("read error:", err)
 		}
 		panic(err)
 	}
-	
-	if err := json.Unmarshal(buffer, &message); err != nil {
+
+	if err := json.Unmarshal(buffer[:bufferLength], &message); err != nil {
 		panic(err)
 	}
-	return 
+	return
 }
